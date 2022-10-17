577454d7af4cc0e7e705b2d9475cc775c7e68764
Okhunov <odil.okhunov.97@gmail.com>
2022-10-18 01:59:26 +0900

    Initial commit

---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Okhunov Odil on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
