7e3257f7b328b5bb455b55eb41057ca09147e0b1
Okhunov <odil.okhunov.97@gmail.com>
2022-11-15 03:27:25 +0900

    password field

--- Password/Password/ViewController.swift
+++ Password/Password/ViewController.swift
@@ -5,15 +5,40 @@
 //  Created by Okhunov Odil on 2022/11/14.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    let stackView = UIStackView()
+    let newPasswordTextField = PasswordTextField(placeHolderText: "New password")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension ViewController{
+    func style(){
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        newPasswordTextField.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func layout(){
+        stackView.addArrangedSubview(newPasswordTextField)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 }
 
